d9ccaf39608b821e4cc698ed8cf60770bf8e5fac
OgarMikeDev <123175953+OgarMikeDev@users.noreply.github.com>
2025-04-14 14:06:00 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,41 +1,47 @@
 import java.time.LocalDate;
-import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         Company firstCompany = new Company();
 
         Employee firstEmployee = new Operator(
-                "Арсений",
-                30_000,
-                LocalDate.of(2020, 4, 9),
+                25_000,
+                LocalDate.of(2020, 9, 20),
+                "Артём",
                 firstCompany
         );
 
         Employee secondEmployee = new Manager(
-                "Борис",
-                40_000,
-                115_000.0 + (int) ((Math.random() * (140_000 - 115_000)) + 1),
-                LocalDate.of(2023, 4, 9),
+                "Борис", 45_000,
+                115_000 + (int) (Math.random() * (140_000-115_000)+ 1),
+                LocalDate.of(2023, 12, 25),
                 firstCompany
         );
 
         Employee thirdEmployee = new TopManager(
-                "Владимир",
-                40_000,
-                LocalDate.of(2023, 4, 9),
+                45_000,
+                LocalDate.of(2023, 12, 25),
+                "Владислав",
                 firstCompany
         );
+
+        for (int i = 0; i < 10; i++) {
+            Employee employee = new Manager(
+                    "Борис", 45_000,
+                    115_000 + (int) (Math.random() * (140_000-115_000)+ 1),
+                    LocalDate.of(2023, 12, 25),
+                    firstCompany
+            );
+            firstCompany.hire(employee);
+        }
 
         firstCompany.hire(firstEmployee);
         firstCompany.hire(secondEmployee);
         firstCompany.hire(thirdEmployee);
+
         System.out.println(firstCompany);
-        System.out.println("Месячная зарплата первого сотрудника \"" + firstEmployee.getName() +
-                "\" равна " + firstEmployee.getMonthSalary());
-        System.out.println("Месячная зарплата второго сотрудника \"" + secondEmployee.getName() +
-                "\" равна " + secondEmployee.getMonthSalary());
-        System.out.println("Месячная зарплата третьего сотрудника \"" + thirdEmployee.getName() +
-                "\" равна " + thirdEmployee.getMonthSalary());
+
+        System.out.println("Топ 3 самых высоких зп: " + firstCompany.getTopSalaryStaff(3));
+        System.out.println("Топ 3 самых низких зп: " + firstCompany.getLowSalaryStaff(3));
     }
 }
