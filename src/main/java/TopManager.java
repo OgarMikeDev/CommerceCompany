7f3ca4011a8b29799b59717d7e4357251b989232
OgarMikeDev <123175953+OgarMikeDev@users.noreply.github.com>
2025-04-14 14:06:54 +0300

    Update TopManager.java

--- src/main/java/TopManager.java
+++ src/main/java/TopManager.java
@@ -1,16 +1,20 @@
 import java.time.LocalDate;
 
 public class TopManager extends Employee {
-    public TopManager(String name, double fixedSalary, LocalDate dateStartWork, Company company) {
-        super(name, fixedSalary, dateStartWork, company);
+    public TopManager(double fixedSalary, LocalDate dateStartWork, String name, Company company) {
+        super(fixedSalary, dateStartWork, name, company);
     }
 
     @Override
     public double getMonthSalary() {
-        double realIncome = getFixedSalary();
+        double realSalary = getFixedSalary();
+
         if (company.getIncomeCompany() > 10_000_000) {
-            realIncome += (getFixedSalary() * 150) / 100;
+            realSalary += getFixedSalary() * 150 / 100;
         }
-        return realIncome;
+
+        return realSalary;
     }
+
+
 }
