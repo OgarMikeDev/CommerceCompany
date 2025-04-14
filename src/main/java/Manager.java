0e727dab23f3719241b18b6c9f453cf633451320
OgarMikeDev <123175953+OgarMikeDev@users.noreply.github.com>
2025-04-14 14:06:23 +0300

    Update Manager.java

--- src/main/java/Manager.java
+++ src/main/java/Manager.java
@@ -2,14 +2,18 @@ import java.time.LocalDate;
 
 public class Manager extends Employee {
     private double moneyEarnedForCompany;
-    public Manager(String name, double fixedSalary, double moneyEarnedForCompany, LocalDate dateStartWork, Company company) {
-        super(name, fixedSalary, dateStartWork, company);
+    public Manager(String name, double fixedSalary,
+                   double moneyEarnedForCompany, LocalDate dateStartWork,
+                   Company company) {
+        super(fixedSalary, dateStartWork, name, company);
         this.moneyEarnedForCompany = moneyEarnedForCompany;
         company.addSumCompany(moneyEarnedForCompany);
     }
 
     @Override
     public double getMonthSalary() {
-        return getFixedSalary() + moneyEarnedForCompany;
+        return getFixedSalary() + ((moneyEarnedForCompany / 100) * 5);
     }
+
+
 }
